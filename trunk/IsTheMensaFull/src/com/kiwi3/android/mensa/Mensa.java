f6343be8e4c2929028f3f145eecf041a5ed371c9
omoling <omoling@89d3be20-c2ab-aca3-2bb1-a4f4903252e8>
2010-10-12 10:14:11 +0000

    Added refresh to menu

--- trunk/IsTheMensaFull/src/com/kiwi3/android/mensa/Mensa.java
+++ trunk/IsTheMensaFull/src/com/kiwi3/android/mensa/Mensa.java
@@ -1,3 +1,21 @@
+ /*  *********************************************************************  **
+ **  Copyright (C) 2010 Omar Moling			 								**
+ **											                                **
+ **  This program is free software; you can redistribute it and/or modify   ** 
+ **  it under the terms of the GNU General Public License as published by   ** 
+ **  the Free Software Foundation; either version 3 of the License, or 		**
+ **	 (at your option) any later version.                                    **
+ **																			**
+ **  This program is distributed in the hope that it will be useful,		** 
+ **  but WITHOUT ANY WARRANTY; without even the implied warranty of 		**
+ **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 			**
+ **	 GNU General Public License for more details.							**
+ **																			**
+ **  You should have received a copy of the GNU General Public License 		**
+ **  along with this program. If not, see <http://www.gnu.org/licenses/>.	**
+ **																			**
+ **  *********************************************************************  */
+
 package com.kiwi3.android.mensa;
 
 import java.io.IOException;
@@ -10,12 +28,17 @@ import android.app.Activity;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 public class Mensa extends Activity {
     private static final String TAG = "Mensa";
 	
     private URL url = null;
+    
+    //MENU
+    private static final int MENU_REFRESH = Menu.FIRST;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,20 +48,20 @@ public class Mensa extends Activity {
         
         try {
         	//sample image
-        	url = new URL("http://isthemensafull.googlecode.com/files/StreamImage.jpeg");
+        	//url = new URL("http://isthemensafull.googlecode.com/files/StreamImage.jpeg");
         	
         	//real url
-        	//url = new URL("http://aws.unibz.it/mensawebcam/StreamImage.aspx");
+        	url = new URL("http://aws.unibz.it/mensawebcam/StreamImage.aspx");
         	
         } catch (MalformedURLException e) {
 			Log.e(TAG, e.getMessage());
 		}
         
-        getImage();
+        refreshImage();
     }
     
-    private void getImage() {
-    	Log.d(TAG, "getImage called");
+    private void refreshImage() {
+    	Log.d(TAG, "refreshImage() called");
 		try {
 			HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
 			httpConnection.setRequestMethod("GET");
@@ -47,12 +70,14 @@ public class Mensa extends Activity {
 			int responseCode = httpConnection.getResponseCode();
 			
 			if (responseCode == HttpURLConnection.HTTP_OK) {
-				Log.w(TAG, "http response code OK");
+				Log.d(TAG, "http response code OK");
 				InputStream in = (InputStream) httpConnection.getInputStream();
 				
 				ImageView image = (ImageView) this.findViewById(R.id.imageview);
 				image.setImageBitmap(BitmapFactory.decodeStream(in));
 				Log.w(TAG, "image set");
+				
+				httpConnection.disconnect();
 			} else {
 				Log.w(TAG, "http response code: " + responseCode);
 			}
@@ -63,4 +88,24 @@ public class Mensa extends Activity {
 		}
     	 
     }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
+    	
+    	menu.add(0, MENU_REFRESH, Menu.NONE, "Refresh");
+    	
+    	return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+		case (MENU_REFRESH):
+			refreshImage();
+			return true;
+		}
+    	return true;
+    }
+    
 }
