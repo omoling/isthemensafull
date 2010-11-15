1e4acffdde3465be4ee4c7851428b5c0ae031e24
omoling <omoling@89d3be20-c2ab-aca3-2bb1-a4f4903252e8>
2010-11-15 21:17:32 +0000

    - added icons for menu entries - added communication logic in asynctask - using indefinite progressbar in titlebar for asynctask

--- trunk/IsTheMensaFull/src/com/kiwi3/android/mensa/Mensa.java
+++ trunk/IsTheMensaFull/src/com/kiwi3/android/mensa/Mensa.java
@@ -25,26 +25,38 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Mensa extends Activity {
     private static final String TAG = "Mensa";
 	
     private URL url = null;
+    private ImageView imageView;
     
     //MENU
     private static final int MENU_REFRESH = Menu.FIRST;
+    private static final int MENU_ABOUT = Menu.FIRST + 1;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
         Log.d(TAG, "Status: onCreate");
+        
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        setContentView(R.layout.main);
+        
+        imageView = (ImageView) this.findViewById(R.id.imageview);
         
         try {
         	//sample image
@@ -62,50 +74,102 @@ public class Mensa extends Activity {
     
     private void refreshImage() {
     	Log.d(TAG, "refreshImage() called");
-		try {
-			HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
-			httpConnection.setRequestMethod("GET");
-			httpConnection.connect();
-			
-			int responseCode = httpConnection.getResponseCode();
-			
-			if (responseCode == HttpURLConnection.HTTP_OK) {
-				Log.d(TAG, "http response code OK");
-				InputStream in = (InputStream) httpConnection.getInputStream();
-				
-				ImageView image = (ImageView) this.findViewById(R.id.imageview);
-				image.setImageBitmap(BitmapFactory.decodeStream(in));
-				Log.w(TAG, "image set");
-				
-				httpConnection.disconnect();
-			} else {
-				Log.w(TAG, "http response code: " + responseCode);
-			}
-		} catch (MalformedURLException e) {
-			Log.e(TAG, e.getMessage());
-		} catch (IOException e) {
-			Log.e(TAG, e.getMessage());
-		}
-    	 
+    	
+    	// call task to refresh the image from the webcam
+		new GetMensaImage().execute();
     }
     
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-    	super.onCreateOptionsMenu(menu);
-    	
-    	menu.add(0, MENU_REFRESH, Menu.NONE, "Refresh");
-    	
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+
+		menu.add(0, MENU_REFRESH, Menu.NONE, R.string.refresh)
+				.setIcon(R.drawable.ic_menu_refresh);
+
+		menu.add(0, MENU_ABOUT, Menu.NONE, R.string.about)
+				.setIcon(R.drawable.ic_menu_info_details);
+
     	return true;
     }
     
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
     	switch (item.getItemId()) {
-		case (MENU_REFRESH):
+		case (MENU_REFRESH): {
 			refreshImage();
 			return true;
 		}
+		case (MENU_ABOUT): {
+			startActivity(new Intent(Intent.ACTION_VIEW, 
+					Uri.parse(getString(R.string.about_url))));
+			return true;
+		}
+		}
     	return true;
+    }
+    
+    private class GetMensaImage extends AsyncTask<Void, Void, Bitmap> {
+
+    	@Override
+    	protected void onPreExecute() {
+    		super.onPreExecute();
+    		setProgressBarIndeterminateVisibility(true);
+    	}
+    	
+		@Override
+		protected Bitmap doInBackground(Void... params) {
+			HttpURLConnection httpConnection = null;
+			
+			try {
+				httpConnection = (HttpURLConnection) url
+						.openConnection();
+				httpConnection.setRequestMethod("GET");
+				httpConnection.connect();
+
+				int responseCode = httpConnection.getResponseCode();
+
+				if (responseCode == HttpURLConnection.HTTP_OK) {
+					InputStream in = (InputStream) httpConnection
+							.getInputStream();
+
+					Bitmap image = BitmapFactory.decodeStream(in);
+
+					return image;
+				} else {
+					Log.w(TAG, "http response code: " + responseCode);
+				}
+			} catch (MalformedURLException e) {
+				Log.e(TAG, e.getMessage());
+			} catch (IOException e) {
+				Log.e(TAG, e.getMessage());
+			} finally {
+				if (httpConnection != null) {
+					httpConnection.disconnect();
+				}
+			}
+			return null;
+		}
+		
+		@Override
+		protected void onPostExecute(Bitmap result) {
+			super.onPostExecute(result);
+			
+			if (result != null) {
+				imageView.setImageBitmap(result);
+				
+				// determine if image is 1x1: means webcam is not available
+				int width = result.getWidth(), height = result.getHeight();
+				Log.d(TAG, "image W x H: " + width + " x " + height);
+				
+				if (width == 1 && height == 1) {
+					Log.d(TAG, "Image is 1x1, webcam not available.");
+					Toast.makeText(getBaseContext(), "The webcam appears to be not available. Please check the instructions.", Toast.LENGTH_LONG).show();
+				}
+			}
+			
+			setProgressBarIndeterminateVisibility(false);
+		}
+    	
     }
     
 }
